37485f5c50be2643b93a83ac81ee5ea865c7f432
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-17 18:16:48 +0300

    add HW4

---
+++ HW4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+System.Console.WriteLine ("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int currentNumber = 1; currentNumber <= number; currentNumber ++ )
+ if (currentNumber % 2==0)
+ {
+     System.Console.Write (currentNumber+ " " );
+ }
+
+
+
+
+
+
+
+
+
+
