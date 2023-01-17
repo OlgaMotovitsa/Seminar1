fef4b92214f25f55d6a482721037697a4751312f
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-17 18:19:11 +0300

    add changes at HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите число");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine("Введите число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 
